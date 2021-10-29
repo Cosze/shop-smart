f821a0af3eb132ad0c4992e177b88431b9cafcfd
Cody Szeto <codyszet@gmail.com>
2021-10-29 00:10:41 -0700

    Initialized styled-components for StyleSelector component

--- page-components/overview/productInfo/styleSelector/styleSelector.js
+++ page-components/overview/productInfo/styleSelector/styleSelector.js
@@ -2,6 +2,11 @@ import * as S from './styleSelector.styles';
 
 export default function StyleSelector() {
   return (
-    <div>StyleSelector</div>
+    <S.Wrapper className='style-selector'>
+      <div>
+        <S.Text>Style</S.Text>
+        <S.Value>White</S.Value>
+      </div>
+    </S.Wrapper>
   );
 }
--- page-components/overview/productInfo/styleSelector/styleSelector.styles.js
+++ page-components/overview/productInfo/styleSelector/styleSelector.styles.js
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Text = styled.span`
+  font-weight: 500;
+  margin-right: 0.7em;
+`;
+
+export const Value = styled.span``;
+
+export const Grid = styled.div`
+  margin-top: 0.5em;
+  display: grid;
+  width: 410px;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+`;
+
+export const ImageContainer = styled.div`
+  width: 75px;
+  height: 75px;
+  border-radius: 50px;
+  overflow: hidden;
+`;
